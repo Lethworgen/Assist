6a2eb33577882c6962d5da1a85015273a50b9b98
SpencerDo <magicdo7@gmail.com>
2016-12-02 14:41:42 -0800

    implements simple json parser.

--- main.cpp
+++ main.cpp
@@ -2,16 +2,25 @@
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 using namespace rapidjson;
 
+const char *parseJsonFile(string path) {
+    ifstream ifs(path);
+    string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+    ifs.close();
+    return content.c_str();
+}
+
 int main() {
-    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
     Document d;
-    d.Parse(json);
-    Value& s = d["stars"];
-    cout << "stars: " << s.GetInt() << endl;
+    d.Parse(parseJsonFile("uc.json"));
+    for (int i = 0; i < d.GetArray().Capacity(); i++) {
+        Value &obj = d.GetArray()[i]["name"];
+        cout << "name[" << (i + 1) << "]: " << obj.GetString() << endl;
+    }
 
     return 0;
 }
