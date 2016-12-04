66f1dec05791fe5835a84ffea403a2fcc52ac69d
SpencerDo <magicdo7@gmail.com>
2016-12-04 03:53:08 -0800

    implements an interface that the user can choose a major for having missing courses.

--- main.cpp
+++ main.cpp
@@ -40,23 +40,72 @@ void showBestMathcedMajors(Parser *parser, SinglyLinkedList<Course> *input,
         }
     }
 
+    int menuNumber = 1;
     for (int i = 0; i < keyList->getNumberOfNodes(); i++) {
-        cout << "university: " << keyList->peek(i).getName() << endl;
+        string univName = keyList->peek(i).getName();
         BST<Major> foundMajors;
         map->get(keyList->peek(i), foundMajors);
-        foundMajors.breadthfirst()->display();
+        Queue<Major> *majors = foundMajors.inorder();
+
+        for (int j = 0; j < majors->getNumberOfNodes(); j++) {
+            Major m = majors->peekFront();
+            majors->dequeue();
+            cout << (menuNumber) << ". " << m.getName() << " (" << univName << ")" << endl;
+            menuNumber++;
+        }
     }
+    cout << endl;
 }
 
+Major getMajor(int menu,
+               SinglyLinkedList<University> *universities,
+               SinglyLinkedList<University> *keyList,
+               HashMap<University, BST<Major>> *map) {
+    for (int i = 0; i < keyList->getNumberOfNodes(); i++) {
+        string univName = keyList->peek(i).getName();
+        BST<Major> foundMajors;
+        map->get(keyList->peek(i), foundMajors);
+        Queue<Major> *majors = foundMajors.inorder();
+
+        for (int j = 0; j < majors->getNumberOfNodes(); j++) {
+            Major m = majors->peekFront();
+            majors->dequeue();
+            menu--;
+            if (menu == 0) {
+                return m;
+            }
+        }
+    }
+    Major tmp;
+    return tmp;
+}
+
+//void createDummyInput(SinglyLinkedList<Course> *input) {
+//    Course *tmpCourse1 = new Course("HIST 17C");
+//    Course *tmpCourse2 = new Course("HIST 17A");
+//    Course *tmpCourse3 = new Course("HIST 17B");
+//    Course *tmpCourse4 = new Course("PHYS 4D");
+//    input->addLast(*tmpCourse1);
+//    input->addLast(*tmpCourse2);
+//    input->addLast(*tmpCourse3);
+//    input->addLast(*tmpCourse4);
+//}
+
 void createDummyInput(SinglyLinkedList<Course> *input) {
-    Course *tmpCourse1 = new Course("HIST 17C");
-    Course *tmpCourse2 = new Course("HIST 17A");
-    Course *tmpCourse3 = new Course("HIST 17B");
-    Course *tmpCourse4 = new Course("PHYS 4D");
-    input->addLast(*tmpCourse1);
-    input->addLast(*tmpCourse2);
-    input->addLast(*tmpCourse3);
-    input->addLast(*tmpCourse4);
+    ifstream fs("taken_courses.txt");
+    if (fs.is_open()) {
+        string s;
+        while (!fs.eof()) {
+            getline(fs, s);
+            Course course(s);
+            input->addLast(course);
+        }
+    } else {
+        cout << "file is not open." << endl;
+    }
+    if (fs.is_open()) {
+        fs.close();
+    }
 }
 
 void showMissingCourses(SinglyLinkedList<Course> *input, BST<Course> *courses) {
@@ -91,12 +140,26 @@ int main() {
     SinglyLinkedList<University> *universities = parser->getUniversities("data.json");
 
     createDummyInput(input);
-    showBestMathcedMajors(parser, input, universities, keyList, map);
 
     // missing courses
-    Major missingCourseMajor = keyList->peek(0).getMajors()->peek(0);
-    showMissingCourses(input, missingCourseMajor.getCourses());
-    writeMissingCourses(missingCourseMajor.getName(), missingCourseMajor.getCourses());
+//    Major missingCourseMajor = keyList->peek(0).getMajors()->peek(0);
+//    showMissingCourses(input, missingCourseMajor.getCourses());
+//    writeMissingCourses(missingCourseMajor.getName(), missingCourseMajor.getCourses());
+
+    cout << "Enter \"stop\" to end the program." << endl;
+    string line = "";
+    while (true) {
+        showBestMathcedMajors(parser, input, universities, keyList, map);
+        cout << "Choose the major to see missing courses: ";
+        cin >> line;
+        if (line == "stop") {
+            break;
+        }
+        int m = stoi(line);
+        Major major = getMajor(m, universities, keyList, map);
+        showMissingCourses(input, major.getCourses());
+        writeMissingCourses(major.getName(), major.getCourses());
+    }
 
     input->clear();
     keyList->clear();
